e655a5968b702e69a292170182e9d2e2beb8b8df
mohammad nabipour <mohammadnabipour1371@gmail.com>
2020-06-28 10:33:28 +0430

    corrected footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,14 +9,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
-    <script src="{{ asset('/assets/files/jquery/jquery-2.1.1.min.js') }}"></script>
-{{--    <script src="{{ asset('/assets/files/bootstrap/css/bootstrap.min.js') }}"></script>--}}
+    <link rel="stylesheet" href="{{asset('assets/bootstrap-3.4.1/css/bootstrap.min.css')}}">
+    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('assets/bootstrap-3.4.1/js/bootstrap.min.js')}}"></script>
     <link href="{{ asset('/assets/files/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
-{{--    <link href="{{ asset('/bootstrap/files/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">--}}
     <script src="{{ asset('/assets/files/common.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/main.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main-front.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fontiran.css') }}">
+
 
 
 
