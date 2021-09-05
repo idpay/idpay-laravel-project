240a5f1844b1f4187e4451fda27ed5086e36fd65
jacob <pourrahmanbr@gmail.com>
2021-09-06 01:05:59 +0430

    Upgrade to laravel 8

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,26 +2,16 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Contracts\Encryption\Encrypter;
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-
-    /**
-     * Indicates whether the XSRF-TOKEN cookie should be set on the response.
-     *
-     * @var bool
-     */
-    protected $addHttpCookie = true;
-
     /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array
      */
     protected $except = [
-        'callback'
+        //
     ];
 }
